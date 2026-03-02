96b46998e7a3a3bb359fac6fcb9c111012bcfd15
Gabriel Trip <tripgabriel30@gmail.com>
2026-03-02 11:21:25 +0200

    rectangle

--- GeoComputaionala/Seminar2/Seminar2/obj/Debug/net9.0-windows/Seminar2.AssemblyInfo.cs
+++ GeoComputaionala/Seminar2/Seminar2/obj/Debug/net9.0-windows/Seminar2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seminar2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2167288bf1c78ef5553bf829887d07dc223455")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seminar2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Seminar2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
