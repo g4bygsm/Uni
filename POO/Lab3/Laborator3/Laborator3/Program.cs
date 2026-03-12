5fbd8c514b3182df22f1bcc7b2251ff17b695446
Gabriel Trip <tripgabriel30@gmail.com>
2026-03-12 10:16:50 +0200

    lma

--- POO/Lab3/Laborator3/Laborator3/Program.cs
+++ POO/Lab3/Laborator3/Laborator3/Program.cs
@@ -1,43 +1,77 @@
-﻿using System;
+﻿//using System;
 
-namespace Lab2
+//namespace Lab2
+//{
+//    internal class Hello
+//    {
+//        // camp static
+//        static int index = 0;
+
+//        //constructor instanta
+//        public Hello()
+//        {
+
+//            Console.WriteLine("Hello World!");
+//        }
+//        // constructor static
+//        static Hello()
+//        {
+//            index++;
+//        }
+//        //destructor
+
+//        ~Hello()
+//        {
+//            Console.WriteLine("Goodbye, World!");
+//        }
+
+//        public static int get_index()
+//        {
+//            return index;
+//        }
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Hello hw1 = new Hello();
+//            Console.WriteLine($"Index = {Hello.get_index()}");
+//            Hello hw2 = new Hello();
+//            Console.WriteLine($"Index = {Hello.get_index()}");
+//        }
+//    }
+//}
+
+// Sa se implementeze o clasa care sa contina operatiile definite
+// ca metode de adunare scadere inmultire impartire a doua nr reale
+
+using System;
+
+namespace proj
 {
-    internal class Hello
+    class NrReale
     {
-        // camp static
-        static int index = 0;
-
-        //constructor instanta
-        public Hello()
+        public double x, y;
+        
+        public NrReale(double x = 0, double y = 0) 
         {
-            
-            Console.WriteLine("Hello World!");
-        }
-        // constructor static
-        static Hello()
-        {
-            index++;
-        }
-        //destructor
-
-        ~Hello()
-        {
-            Console.WriteLine("Goodbye, World!");
+            this.x = x;
+            this.y = y;
         }
 
-        public static int get_index()
+        public double suma()
         {
-            return index;
+            return x + y;
         }
     }
-    class Program
+
+    internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Hello hw1 = new Hello();
-            Console.WriteLine($"Index = {Hello.get_index()}");
-            Hello hw2 = new Hello();
-            Console.WriteLine($"Index = {Hello.get_index()}");
+            NrReale n = new NrReale(20.2, 30.3);
+            n.x = 5;
+            Console.WriteLine(n.suma());
         }
     }
 }
