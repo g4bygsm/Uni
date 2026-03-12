9a5a7246a9109cd8e1d72fd21e0dd82d6628eb31
Gabriel Trip <tripgabriel30@gmail.com>
2026-03-12 09:30:35 +0200

    c2

--- POO/Lab3/Laborator3/Laborator3/Program.cs
+++ POO/Lab3/Laborator3/Laborator3/Program.cs
@@ -1,4 +1,20 @@
-﻿static void P1()
-{
+﻿using System;
 
+namespace Lab2
+{
+    internal class Hello
+    {
+
+        public Hello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello h = new Hello();
+        }
+    }
 }
