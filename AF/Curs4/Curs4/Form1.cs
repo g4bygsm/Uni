a25718580dca945b6099e970e138c29b5ca2d628
Gabriel Trip <tripgabriel30@gmail.com>
2026-03-17 13:32:07 +0200

    new 2

--- AF/Curs4/Curs4/Form1.cs
+++ AF/Curs4/Curs4/Form1.cs
@@ -13,3 +13,28 @@ namespace Curs4
         }
     }
 }
+
+
+// Fibbonaci
+// 
+//int[] x = new x;
+//int k = 32;
+//for(int i = 3; i <= k; i++)
+//{
+//    x[i] = x[i - 1] + x[i - 2];
+//}
+//bool isFibonacci(int y)
+//{
+//    for(int i = 1; i <= k; i++)
+//    {
+//        if (x[i] == y)
+//        {
+//            return true;
+//        }
+//    }
+//    return false;
+//}
+
+
+// x e sau nu puterea lui 2
+// if( x & (x + 1) == 0)
