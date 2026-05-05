07f2fe71b9cfd62286950b1fd76923f506c37947
Gabriel Trip <tripgabriel30@gmail.com>
2026-05-05 13:40:26 +0300

    da

---
+++ AF/StackQueue1/StackQueue1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Se da o matrice de valori intregi. Determinati dimensiunea celui mai mare platou
+// Prin platou se intelege o colectie de elemente invecinate NordSudEstVest de aceeasi valoare x0
+
+// 2 2 3 3 3 
+// 0 5 5 3 1 
+// 0 2 2 2 1 
+// 2 4 0 0 1
+// 3 3 3 2 2
+// 4 3 1 2 3
+// 3 3 1 2 3
+
+  // raspuns 6
