806f6905f1c050e6d0eb53e1858fc8c7c148c737
Gabriel Trip <tripgabriel30@gmail.com>
2026-05-19 13:27:49 +0300

    MergeSort

--- AF/Capitolul7/Capitolul7/Program.cs
+++ AF/Capitolul7/Capitolul7/Program.cs
@@ -1,4 +1,6 @@
-﻿bool BinarySearch(int[] v, int st, int dr, int x)
+﻿using System.Numerics;
+
+bool BinarySearch(int[] v, int st, int dr, int x)
 {
     if (st <= dr)
     {
@@ -18,8 +20,39 @@
 // Merge Sort (interclasare)
 // Quick Sort (sortare rapida) -- examen (?)
 
-int[] v1 = new int[10];
-int[] v2 = new int[10];
-int k1;
-int k2;
 
+
+// Verificăm care element este mai mic și îl punem în noul vector v3
+if (v1[k1] < v2[k2])
+{
+    v3[k3] = v1[k1];
+    k1++; // Trecem la următorul element din v1
+    k3++; // Trecem la următoarea poziție liberă din v3
+}
+else
+{
+    v3[k3] = v2[k2];
+    k2++; // Trecem la următorul element din v2
+    k3++;
+}
+
+// Dacă s-au terminat elementele din v2, dar au mai rămas în v1, le copiem direct
+while (k1 < m1)
+{
+    v3[k3] = v1[k1];
+    k1++;
+    k3++;
+}
+
+// Alternativ, dacă s-au terminat în v1, dar au mai rămas în v2
+while (k2 < m2)
+{
+    v3[k3] = v2[k2];
+    k2++;
+    k3++;
+}
+
+// PROBLEMA
+// Se dau 4 tije ABCD se considera A sursa, D destinatie iar B si C intermediare in contextul problemei 
+// turnurilor din Harmon. Rezolvati un algoritm care muta n discuri de pe sursa pe destinatie. Considerati
+// o solutie viabila cu un nr de pasi mai mic decat echivalentul.
