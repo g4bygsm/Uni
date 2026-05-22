0f88de54c4c3e22c926874b1b698a1c0c7a5c9f5
Gabriel Trip <tripgabriel30@gmail.com>
2026-05-22 13:27:42 +0300

    Factorial

---
+++ AF/Proiecte de Laborator/Factorial/Factorial/Class1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Factorial
+{
+    public class BigNumber
+    {
+        private List<int> digits;
+        public int length;
+
+        // Constructor implicit
+        public BigNumber()
+        {
+            length = 0;
+            digits = new List<int>();
+        }
+
+        // Constructor dintr-un numar intreg (int)
+        public BigNumber(int original)
+        {
+            digits = new List<int>();
+
+            if (original == 0)
+            {
+                Add(0);
+            }
+            else
+            {
+                while (original > 0)
+                {
+                    Add(original % 10);
+                    original /= 10;
+                }
+                digits.Reverse();
+            }
+        }
+
+        // Constructor dintr-o lista de cifre (Creeaza o copie sigura)
+        public BigNumber(List<int> digits, int length)
+        {
+            this.digits = new List<int>(digits);
+            this.length = length;
+        }
+
+        // Indexator pentru a citi cifrele direct prin obiect[i]
+        public int this[int index]
+        {
+            get { return digits[index]; }
+            private set { digits[index] = value; }
+        }
+
+        // Operatorul de adunare (+) - REPARAT COMPLET
+        public static BigNumber operator +(BigNumber a, BigNumber b)
+        {
+            BigNumber result = new BigNumber();
+            int carry = 0;
+
+            int i = a.length - 1;
+            int j = b.length - 1;
+
+            // Adunam cifrele de la dreapta la stanga
+            while (i >= 0 || j >= 0 || carry > 0)
+            {
+                int sum = carry;
+                if (i >= 0) sum += a[i--];
+                if (j >= 0) sum += b[j--];
+
+                result.Add(sum % 10);
+                carry = sum / 10;
+            }
+
+            result.Reverse();
+            return result;
+        }
+
+        // Operatorul de inmultire (*) - REPARAT COMPLET
+        public static BigNumber operator *(BigNumber a, BigNumber b)
+        {
+            // Daca unul dintre numere este 0, rezultatul inmultirii este 0
+            if ((a.length == 1 && a[0] == 0) || (b.length == 1 && b[0] == 0))
+                return new BigNumber(0);
+
+            BigNumber result = new BigNumber(0);
+
+            // Inmultim fiecare cifra din b cu intregul numar a
+            for (int i = b.length - 1; i >= 0; i--)
+            {
+                int digitB = b[i];
+                if (digitB == 0) continue;
+
+                BigNumber partial = new BigNumber();
+                int carry = 0;
+
+                for (int j = a.length - 1; j >= 0; j--)
+                {
+                    int prod = (a[j] * digitB) + carry;
+                    partial.Add(prod % 10);
+                    carry = prod / 10;
+                }
+
+                if (carry > 0)
+                    partial.Add(carry);
+
+                partial.Reverse();
+
+                // Adaugam zerouri la coada in functie de ordinul cifrei (zeci, sute...)
+                int zerosCount = b.length - 1 - i;
+                for (int k = 0; k < zerosCount; k++)
+                {
+                    partial.Add(0);
+                }
+
+                // Adunam rezultatul partial la cel final
+                result = result + partial;
+            }
+
+            return result;
+        }
+
+        // Metode ajutatoare (Helper functions)
+        public void Reverse()
+        {
+            digits.Reverse();
+        }
+
+        public void Add(int digit)
+        {
+            digits.Add(digit);
+            length++;
+        }
+
+        public override string ToString()
+        {
+            string result = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                result += this[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ AF/Proiecte de Laborator/Factorial/Factorial/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // =================================================================
+            // 1. CODUL DE LA CURS (Matrice, Factorial, Fibonacci)
+            // =================================================================
+
+            // Declarare Matrice
+            Random random = new Random();
+            int n = 10, m = 8;
+            int[,] matrix = new int[n, m];
+
+            // Valori aleatorii
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    matrix[i, j] = random.Next(1, 10);
+
+            // Extindere + afisare
+            Console.WriteLine("--- Matricea Extinsa ---");
+            matrix = Extindere(matrix);
+            for (int i = 0; i < 2 * n - 1; i++)
+            {
+                for (int j = 0; j < 2 * m - 1; j++)
+                    Console.Write(matrix[i, j] + " ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n--- Fibonacci (pozitia 400) ---");
+            Console.WriteLine(Fibonacci(400));
+
+            // Nota: Factorial(5000) dureaza extrem de mult (minute bune) din cauza 
+            // recursivitatii adanci pe numere mari. Daca vrei sa testezi rapid, 
+            // poti lasa codul asa sau sa pui o valoare mai mica, de exemplu Factorial(100).
+            Console.WriteLine("\n--- Calcul 5000! ---");
+            Console.WriteLine("Se calculeaza 5000!... (Apasati Ctrl+C daca vreti sa treceti peste)");
+            Console.WriteLine(Factorial(5000));
+
+
+            // =================================================================
+            // 2. REZOLVARE CERINTA EXERCIȚIULUI 4: Expresia E = suma(i^i) de la 1 la 255
+            // =================================================================
+            Console.WriteLine("\n==================================================");
+            Console.WriteLine("INCEPEM CALCULUL EXPRESIEI E PENTRU TEMA...");
+            Console.WriteLine("==================================================");
+
+            BigNumber sumaE = new BigNumber(0);
+
+            for (int i = 1; i <= 255; i++)
+            {
+                sumaE = sumaE + Power(i, i);
+
+                // Afisam progresul din 50 in 50 ca sa stii ca programul lucreaza activ
+                if (i % 50 == 0 || i == 255)
+                {
+                    Console.WriteLine($"[Progres] Calculat pana la termenul: {i}^{i}");
+                }
+            }
+
+            Console.WriteLine("\n>>> CALCUL FINALIZAT CU SUCCES! <<<");
+            Console.WriteLine("Rezultatul complet al expresiei E este urmatorul numar urias:");
+            Console.WriteLine(sumaE);
+
+            // ACEASTA LINIE IMPIEDICA CONSOLA SA SE INCHIDA SINGURA!
+            Console.WriteLine("\nProgramul s-a terminat. Apasati tasta ENTER pentru a inchide fereastra...");
+            Console.ReadLine();
+        }
+
+        // Metoda de la curs: Calculati n! recursiv
+        static BigNumber Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+                return new BigNumber(1);
+            return new BigNumber(n) * Factorial(n - 1);
+        }
+
+        // Metoda de la curs: Calculati elementul de pe pozitia n din sirul lui Fibonacci
+        static BigNumber Fibonacci(int n)
+        {
+            BigNumber[] fibonacci = new BigNumber[n];
+            fibonacci[0] = new BigNumber(1);
+            fibonacci[1] = new BigNumber(1);
+            for (int i = 2; i < n; i++)
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            return fibonacci[n - 1];
+        }
+
+        // Metoda de la curs: Extindere matrice cu medii aritmetice
+        static int[,] Extindere(int[,] matrix)
+        {
+            int n = matrix.GetLength(0);
+            int m = matrix.GetLength(1);
+
+            int[,] Matrix = new int[2 * n - 1, 2 * m - 1];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    Matrix[2 * i, 2 * j] = matrix[i, j];
+                    if (j > 0)
+                    {
+                        Matrix[2 * i, 2 * j - 1] = (matrix[i, j - 1] + matrix[i, j]) / 2;
+                    }
+                }
+
+            for (int i = 1; i < 2 * n - 1; i += 2)
+                for (int j = 0; j < 2 * m - 1; j++)
+                {
+                    Matrix[i, j] = (Matrix[i - 1, j] + Matrix[i + 1, j]) / 2;
+                }
+            return Matrix;
+        }
+
+        // Metoda OPTIMIZATA (Ridicarea la putere in timp logaritmic)
+        static BigNumber Power(int baseNum, int exponent)
+        {
+            BigNumber result = new BigNumber(1);
+            BigNumber baseBig = new BigNumber(baseNum);
+
+            while (exponent > 0)
+            {
+                if (exponent % 2 == 1)
+                {
+                    result = result * baseBig;
+                }
+                baseBig = baseBig * baseBig;
+                exponent /= 2;
+            }
+
+            return result;
+        }
+    }
+}
